7518f130299943dfbd0cc1b82b291b7304142465
mac-eth <macsweenydev@gmail.com>
2022-06-16 12:50:54 +1000

    About

--- components/About.jsx
+++ components/About.jsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react';
 
 const About = () => {
@@ -10,23 +11,19 @@ const About = () => {
           </p>
           <h2 className='py-4'>Who I Am</h2>
           <p className='py-2 text-gray-600'>
-            // I am not your average developer
+            In 2018 I started working with Node.js and c++ to make some modifications to a 
+            discord bot and some gaming applications. What I thought were minor edits on 
+            some basic applications turned into a love for programming. This love for 
+            programming then transpired to blockchain technologies such as 
+            decentralized applications and decentralized financial systems. 
           </p>
           <p className='py-2 text-gray-600'>
-            I have spent the last 12 years in the fire service working as a
-            professional firefighter & paramedic. I have always had a knack for
-            technology and working with computers. In 2019 I started working
-            with HTML & CSS to make some minor edits on a small business website
-            that I was operating. What I thought was just a few small edits
-            turned into a love for programming.
-          </p>
-          <p className='py-2 text-gray-600'>
-            Fascinated with how intricate programming can be I was quickly drawn
-            to learn more. I started learning javascript and was even more
-            enthused with making websites interactive. I then started
-            freelancing for e-commerce companies on the Shopify platform. I am
-            now spending my time building projects with React JS, Firebase, and
-            learning new technologies.
+            Fascinated with how blockchain will revolutionize the state of technology, I 
+            started learning the basics of solidity and web3.js. I was very quickly drawn
+            to learn more and enthused with solving problems that traditional applications
+            have not been able to do. I then started working for several different blockchain
+            companies. In my spare time I have been designing decentralized applications 
+            which have the capability of solving complex, real world issues.
           </p>
           <a href='/#projects'>
             <p className='py-2 text-gray-600 underline cursor-pointer'>
@@ -35,11 +32,12 @@ const About = () => {
           </a>
         </div>
         <div className='w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
-          <img
-            className='rounded-xl'
-            src='https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1364&q=80'
-            alt='/'
-          />
+        <Image
+                  src='/../public/assets/about/about.png'
+                  width='1200px'
+                  height='570px'
+                  alt='/'
+                />
         </div>
       </div>
     </div>
